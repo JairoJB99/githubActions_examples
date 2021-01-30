5acc3a3bfb5d3a7e279dcbf1d8d0cfd230947b92
JairoJB99 <jairocnx@gmail.com>
2021-01-30 09:52:41 +0100

    Telegram Upgrades

--- .github/actions/send_telegram_action/index.js
+++ .github/actions/send_telegram_action/index.js
@@ -1,14 +1,20 @@
 const core = require("@actions/core");
-
+const github = require("@actions/github")
 const TelegramBot = require('node-telegram-bot-api');
 
+let git = github.context.payload; 
 const token = core.getInput('token_telegram');
 const bot = new TelegramBot(token, { polling: false });
 const chatId = core.getInput('chat_id');
 const username = core.getInput('username')
 
 try {
-  bot.sendMessage(chatId, `Workflow ejecutado correctamente tras el último commit. Saludos ${username}`);
+  bot.sendMessage(chatId, `Workflow ejecutado correctamente tras el último commit. Saludos ${username}
+                            COMMIT INFO: /n
+                            Name: ${git.head_commit.author.name} /n
+                            Username: ${git.head_commit.author.username} /n
+                            Email: ${git.head_commit.author.email} /n
+                            Message_Commit: ${git.head_commit.message} /n`);
 } catch (error) {
   core.setFailed(error.message);
 }
